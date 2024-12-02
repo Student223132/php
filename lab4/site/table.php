56cab5a39447808520aef378959c580cfd9601bc
Student223132 <161282535+Student223132@users.noreply.github.com>
2024-12-02 14:56:50 +0300

    lab4 fix

--- lab4/site/table.php
+++ lab4/site/table.php
@@ -10,7 +10,7 @@
       <br>
       <label>Цвет: </label>
       <br>
-      <input name='color' type='color' value='<?php if (isset($_POST['color'])) echo $_POST['color'] ?>' list="listColors">
+      <input name='color' type='color' value='<?php if (isset($_POST['color'])){ echo $_POST['color'];} else echo '#fae3a2'; ?>' list="listColors">
 	<datalist id="listColors">
 		<option>#fae3a2</option>
 		<option>#8de39f</option>
@@ -28,12 +28,11 @@
 	        $rows = abs((int) $_POST['rows']);
 	        $color = trim(strip_tags($_POST['color']));
         }
-        $cols = ($cols) ? $cols : 10;
-        $rows = ($rows) ? $rows : 10;
-        $color = ($color) ? $color : '#ffff00';
-        
         if(isset($cols, $rows, $color)){
-          drawTable($cols, $rows, $color);  
+            $cols = ($cols) ? $cols : 10;
+            $rows = ($rows) ? $rows : 10;
+            $color = ($color) ? $color : '#ffff00';
+            drawTable($cols, $rows, $color);  
         }
         
     ?>
